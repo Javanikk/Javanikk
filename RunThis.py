abfb692bf7511bbfcdc42b48c5565b4182f288e1
Javier <165859981+Javanikk@users.noreply.github.com>
2024-05-03 22:34:48 -0400

    Added a file
    If you run this file, you can select a language and then see a welcome message. I should, although, thanks DeepL for some translations, especially for Deutsch and 日本語

---
+++ RunThis.py
@@ -0,0 +1,26 @@
+print("Select 1")
+print("1.Español\n2.English\n3.Deutsch\n4.日本語\n5.Potuguês\n")
+idioma = int(input())
+# se detecta el idioma
+
+if idioma==1:
+    print("¡Bienvenido a mi página!")
+    print("  ")
+    print("Esta página sirve para mis proyectos públicos, por lo que aquí\nverás, probablemente, de todo un poco lo que te puedas imaginar")
+elif idioma==2:
+    print("Welcome to my page!")
+    print("  ")
+    print("This page is for my public projects, so here you'll probably see\na bit of everything you can imagine")
+elif idioma==3:
+    print("Willkommen auf meiner Website!")
+    print("  ")
+    print("Diese Seite ist für meine öffenlichten Projekte gedacht,\n also werden Sie hier wahrscheinlich ein bisschen von allem sehen, was Sie sich vorstellen können")
+elif idioma==4:
+    print("僕のウエブサイトへようこそ！")
+    print("  ")
+    print("このページは私の公開プロジェクトのためのものなので、おそらくここで、4\nあなたが想像できるあらゆるものを少し見ることができるだろう")
+elif idioma==5:
+    print("¡Bem-vindo na minha página!")
+    print("  ")
+    print("Esta página é para meus projetos públicos, portanto, aqui você provavelmente verá\num pouco de tudo que possa imaginar")
+# el programa reacciona al número dado por el usuario
