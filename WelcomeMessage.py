65df3862c29cc6db14e8c4bd01028af9a0f2ba97
Javier <165859981+Javanikk@users.noreply.github.com>
2025-07-19 21:32:24 -0400

    Update WelcomeMessage.py
    Changed the if-else for a match

--- WelcomeMessage.py
+++ WelcomeMessage.py
@@ -1,23 +1,27 @@
 print("Select 1")
 print("1.Español\n2.English\n3.Deutsch\n4.日本語\n5.Potuguês\n")
 idioma = int(input())
-# se detecta el idioma
+# Se detecta el idioma
 
-if idioma==1:
-    print("¡Bienvenido a mi página!\n")
-    print("Esta página sirve para mis proyectos públicos, por lo que aquí\nverás, probablemente, de todo un poco lo que te puedas imaginar")
-elif idioma==2:
-    print("Welcome to my page!\n")
-    print("This page is for my public projects, so here you'll probably see\na bit of everything you can imagine")
-elif idioma==3:
-    print("Willkommen auf meiner Website!\n")
-    print("Diese Seite ist für meine öffenlichten Projekte gedacht,\n also werden Sie hier wahrscheinlich ein bisschen von allem sehen, was Sie sich vorstellen können")
-elif idioma==4:
-    print("僕のウエブサイトへようこそ！\n")
-    print("このページは私の公開プロジェクトのためのものなので、おそらくここで、\nあなたが想像できるあらゆるものを少し見ることができるだろう")
-elif idioma==5:
-    print("¡Bem-vindo na minha página!\n")
-    print("Esta página é para meus projetos públicos, portanto, aqui você provavelmente verá\num pouco de tudo que possa imaginar")
-# el programa reacciona al número dado por el usuario
+match idioma:
+    case 1:
+        print("¡Bienvenido a mi página!\n")
+        print("Esta página sirve para mis proyectos públicos, por lo que aquí\nverás, probablemente, de todo un poco lo que te puedas imaginar")
+    case 2:
+        print("Welcome to my page!\n")
+        print("This page is for my public projects, so here you'll probably see\na bit of everything you can imagine")
+    case 3:
+        print("Willkommen auf meiner Website!\n")
+        print("Diese Seite ist für meine öffenlichten Projekte gedacht,\n also werden Sie hier wahrscheinlich ein bisschen von allem sehen, was Sie sich vorstellen können")
+    case 4:
+        print("僕のウエブサイトへようこそ！\n")
+        print("このページは私の公開プロジェクトのためのものなので、おそらくここで、\nあなたが想像できるあらゆるものを少し見ることができるだろう")
+    case 5:
+        print("¡Bem-vindo na minha página!\n")
+        print("Esta página é para meus projetos públicos, portanto, aqui você provavelmente verá\num pouco de tudo que possa imaginar")
+    case _:
+        print("ERROR: NON-VALID OPTION")
+
+# El programa reacciona al número dado por el usuario
 
 # Traducciones para alemán y japonés hechas gracias a DeepL
